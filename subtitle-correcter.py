54b596f444bd34ad7b95d45e46001c46f4dece52
anil zeybek <anil.zeybek98@gmail.com>
2019-10-17 19:31:51 +0300

    add support for multiple files and directory

--- subtitle-correcter.py
+++ subtitle-correcter.py
@@ -1,10 +1,5 @@
+import os
 import sys
-
-
-def check_argv():
-    if len(sys.argv) < 2:
-        print('At least 1 .srt file in arguments')
-        sys.exit(0)
 
 
 def change_content(file_name):
@@ -12,15 +7,32 @@ def change_content(file_name):
         print('Not a .srt file')
         sys.exit(0)
 
-    with open(file_name, 'r') as f:
-        text = f.read()
-    text.encode('utf-8')
+    try:
+        with open(file_name, 'r') as f:
+            text = f.read()
+        text.encode('utf-8')
 
-    with open(file_name, 'w', encoding='utf-8') as f:
-        f.write(text)
+        with open(file_name, 'w', encoding='utf-8') as f:
+            f.write(text)
+
+        print(f'DONE {file_name}')
+    except UnicodeDecodeError:
+        print(f'Passing {file_name}')
+
+
+def filter_srt(file_name):
+    return file_name[-4:] == '.srt'
 
 
 if __name__ == '__main__':
-    check_argv()
-    change_content(file_name=sys.argv[1])
-    print('DONE!')
+    all_files = [f for f in os.listdir('.') if os.path.isfile(f)]
+
+    srt_files_argv = list(filter(filter_srt, sys.argv))
+    srt_files_all = list(filter(filter_srt, all_files))
+
+    if sys.argv[1] == '.':
+        for f_name in srt_files_all:
+            change_content(f_name)
+    elif sys.argv[1][-4:] == '.srt':
+        for f_name in srt_files_argv:
+            change_content(f_name)
